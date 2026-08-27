ad008023c39e6b0d393f5703bdc918e0436a2817
Pavel Potapov <paveln0366@gmail.com>
2026-08-27 12:22:50 +0300

    Add coil dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,7 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.squareup.retrofit2:converter-kotlinx-serialization:3.0.0")
+
+    implementation("io.coil-kt.coil3:coil-compose:3.6.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.6.0")
 }
